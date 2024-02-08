755d25cdb5bfe5629915967999044aac21505796
Pedro <pedro_mb_silva@estudante.sesisenai.org.br>
2024-02-08 10:20:02 -0300

    arquivos do programa

--- voto_main.js
+++ voto_main.js
@@ -5,4 +5,6 @@ verificar.addEventListener('click', ()=>{
     let nome = document.getElementById('nome').value
     let idade = Number(document.getElementById('idade').value)
     let titulo = Number(document.getElementById('titulo').value)
+
+    console.log(nome,idade,titulo)
 })
